72d7de6acc8eb25c0c517bdb3363a191f4cffc01
khaled khattab <91126212+Khattab222@users.noreply.github.com>
2023-10-12 23:07:40 +0300

    send email

--- src/app/api/users/signup/route.js
+++ src/app/api/users/signup/route.js
@@ -3,6 +3,8 @@ import User from '@/models/userModel'
 import {NextRequest,NextResponse} from 'next/server'
 import bcryptjs from 'bcryptjs'
 import { connectionDb } from '@/dbConfig/connection'
+import  jwt  from 'jsonwebtoken';
+import sendEmail from '@/helpers/mailer';
 
 
 
@@ -20,16 +22,29 @@ export async function POST (request){
          return   NextResponse.json({message:'user already exist'},{cause:400})
         }
         const hashPassword = bcryptjs.hashSync(password,6);
-        const newUser = User.create({
+        const newUser =new User({
             username,
             email,
             password:hashPassword
         })
-        if (!newUser) {
-            return   NextResponse.json({message:'error in create new user'},{cause:400})
+       
+        // generate token to confirm email
+        const token =await jwt.sign({id:newUser._id},process.env.TOKEN_KEY,{expiresIn:'1d'});
+        // confirm link
+        const confirmLink = `${process.env.DOMAIN}/confirmemail?token=${token}`
+        // send mail with link for confirmation
+        const sendemail = await sendEmail({
+            to:newUser.email,
+            message:`<a href=${confirmLink}>click to confirm email</a>`,
+            subject:"confirm you Email",
+            
+        })
+        if (!sendemail) {
+        throw new Error("unknown error please try again")
+            
         }
-        console.log(newUser)
-        return NextResponse.json({message:'user created success'},{cause:201})
+  
+        return NextResponse.json({message:'user created success please confirm from your mail'},{cause:201})
 
     } catch (error) {
         return NextResponse.json({error:error.message},{status:500})
